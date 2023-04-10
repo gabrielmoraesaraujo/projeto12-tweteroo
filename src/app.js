e52529609d0d66822cfa100db67e67718549ca40
Gabriel Moraes <gmoraes.mat@gmail.com>
2023-04-10 02:43:48 -0300

    Implementação da rota get (tweets)

--- src/app.js
+++ src/app.js
@@ -38,13 +38,15 @@ app.get("/tweets", (req, res) => {
 
     for(let i=0; i<tweets.length; i++){
         for(let j=0; j<users.length; j++){
-            if(user[j].username === tweets [i].username){
-                unity.push({})
+            if(users[j].username === tweets [i].username){
+                unity.push({username:users[j].username, avatar:users[j].avatar, tweet:tweets[i].tweet})
             }
         }
     }
 
-    res.send(tweets)
+    const reverse_unity = unity.reverse()
+
+    res.status(200).send(reverse_unity.slice(0,10))
 
 })
 
